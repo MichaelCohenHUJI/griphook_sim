a10edfe448d0d1de6889ff0ab1d5f2f537357fc0
MichaelCohenHUJI <michael.cohen3@mail.huji.ac.il>
2022-09-02 00:15:43 +0300

    added some plots and single tx propagation

--- sim.py
+++ sim.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 from tqdm import tqdm
 from scipy import stats
+import matplotlib.pyplot as plt
 
 
 def create_adj_mat():
@@ -26,6 +27,13 @@ def create_adj_mat():
             M[l, i] = 1
             num_cons[l] += 1
             num_cons[i] += 1
+    plt.hist(num_cons, 128, density=True, facecolor='g', alpha=0.75)
+    plt.xlabel('number of connections')
+    plt.ylabel('Probability')
+    plt.title('Histogram of Connections number')
+    # plt.xlim(0, 136)
+    plt.grid(True)
+    plt.show()
     return M
 
 
@@ -43,6 +51,7 @@ def one_step(A: set, B: set, adj_mat: np.array, simultanous: bool):
     joint = tempA.intersection(tempB)
     tempA = tempA - joint
     tempB = tempB - joint
+
     # for each joint element choose by coin flip
     if simultanous:
         for elem in joint:
@@ -51,12 +60,18 @@ def one_step(A: set, B: set, adj_mat: np.array, simultanous: bool):
     else:
         tempA.update(joint)
     # todo add joint elements always to A when worst case?
+
     A.update(tempA)
     B.update(tempB)
     return A, B
 
 
 def simulation(max_dif=False, simultanous=True):
+    if max_dif:
+        print("worst case mode")
+    else:
+        print("random mode")
+
     # create adj matrix
     adj_mat = create_adj_mat()
     print("finished computing adjacency matrix")
@@ -74,6 +89,8 @@ def simulation(max_dif=False, simultanous=True):
     print()
 
     # add neutral nodes
+    ansfota = [1]
+    ansfotb = [1]
     end = False
     counter = 0
     while not end:
@@ -82,18 +99,21 @@ def simulation(max_dif=False, simultanous=True):
         A, B = one_step(A, B, adj_mat, simultanous)
         if sizeA == len(A) and sizeB == len(B):
             end = True
+        else:
+            ansfota.append(len(A) - sizeA)
+            ansfotb.append(len(B) - sizeB)
         if len(A) + len(B) > counter + 1000:
             percentage = int((len(A) + len(B)) * 100 / n)
             print(str(percentage) + "% of nodes covered")
-    print()
-    print("number of nodes: " + str(n))
-    if max_dif:
-        print("worst case mode")
-    else:
-        print("random mode")
-    print("initial set size: " + str(set_size))
-    print("node self initiated connections: " + str(initiate_con))
-    print("max connections per node: " + str(max_connections))
+    plt.plot(ansfota, label="A")
+    plt.plot(ansfotb, label="B")
+    plt.xlabel('t')
+    plt.ylabel('added nodes')
+    plt.title('# of new nodes in each time step')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
     _, minmax, avg_cons, var_cons, __, ___, = stats.describe(num_cons)
     print("avg connections per node: " + str(avg_cons))
     print("var of connections per node: " + str(var_cons))
@@ -108,16 +128,64 @@ def simulation(max_dif=False, simultanous=True):
     return
 
 
+def single_tx_propagation():
+    adj_mat = create_adj_mat()
+    starting_node = random.randint(0, n - 1)
+    A = {starting_node}
+    # add neutral nodes
+    end = False
+    counter = 0
+    added_as_func_of_time = [1]
+    while not end:
+        sizeA = len(A)
+        tempA = set()
+        for node in A:
+            curA = set(np.argwhere(adj_mat[node] == 1).flatten())
+            tempA.update(curA)
+        A.update(tempA)
+        if sizeA == len(A):
+            end = True
+        else:
+            added_as_func_of_time.append(len(A) - sizeA)
+        if len(A) > counter + 1000:
+            percentage = int(len(A) * 100 / n)
+            print(str(percentage) + "% of nodes covered")
+    print(added_as_func_of_time)
+    plt.plot(added_as_func_of_time)
+    plt.xlabel('t')
+    plt.ylabel('added nodes')
+    plt.title('# of new nodes in each time step')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+
+    _, minmax, avg_cons, var_cons, __, ___, = stats.describe(num_cons)
+    print("avg connections per node: " + str(avg_cons))
+    print("var of connections per node: " + str(var_cons))
+    print("min connections per node: " + str(minmax[0]))
+    print("max connections per node: " + str(minmax[1]))
+    print()
+    print("-------------------------------------------------------------")
+
+
 
 if __name__ == '__main__':
-    n = 10000
+    n = 100000
     set_size = 1
     max_connections = 128
     num_cons = np.zeros(n)
-    initiate_con = 16
+    initiate_con = 8
+    print()
+    print("number of nodes: " + str(n))
+    print("initial set size: " + str(set_size))
+    print("node self initiated connections: " + str(initiate_con))
+    print("max connections per node: " + str(max_connections))
     simulation(max_dif=False, simultanous=True)
     print("********************* started sim #2 *********************")
     # reset connection numbers
     num_cons = np.zeros(n)
-    simulation(max_dif=False, simultanous=False)
+    simulation(max_dif=True, simultanous=True)
+    # num_cons = np.zeros(n)
+    # single_tx_propagation()
 
